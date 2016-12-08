524f07f9f6ac3c2b95bd16babf1cc3e4a74cd859
Remus Buzatu <r.buzatu90@gmail.com>
2016-12-08 10:24:24 +0200

    Moved iniset from local.sh to run-all-tests.sh (#127)
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- devstack_vm/devstack/local.sh
+++ devstack_vm/devstack/local.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-source /home/ubuntu/devstack/functions
-source /home/ubuntu/devstack/functions-common
-
 echo "Before updating nova flavors:"
 nova flavor-list
 
@@ -14,9 +11,7 @@ if [ "$branch" == "stable/newton" ] || [ "$branch" == "stable/liberty" ] || [ "$
 fi
 
 nova flavor-create m1.nano 42 128 1 1
-
 nova flavor-create m1.micro 84 128 2 1
-
 nova flavor-create m1.heat 451 512 5 1
 
 echo "After updating nova flavors:"
@@ -33,26 +28,3 @@ echo "Neutron subnetworks:"
 neutron subnet-list
 for subnet in `neutron subnet-list -F name | grep -v '\-\-' | grep -v "name" | awk {'print $2'}`; do neutron subnet-show $subnet; done
 
-TEMPEST_CONFIG=/opt/stack/tempest/etc/tempest.conf
-
-iniset $TEMPEST_CONFIG compute volume_device_name "sdb"
-iniset $TEMPEST_CONFIG compute-feature-enabled rdp_console true
-iniset $TEMPEST_CONFIG compute-feature-enabled block_migrate_cinder_iscsi True
-iniset $TEMPEST_CONFIG compute-feature-enabled block_migration_for_live_migration True
-iniset $TEMPEST_CONFIG compute-feature-enabled live_migration True
-iniset $TEMPEST_CONFIG compute-feature-enabled interface_attach False
-
-iniset $TEMPEST_CONFIG scenario img_dir "/home/ubuntu/devstack/files/images/"
-iniset $TEMPEST_CONFIG scenario img_file "cirros-0.3.4-x86_64.vhdx"
-iniset $TEMPEST_CONFIG scenario img_disk_format vhd
-
-IMAGE_REF=`iniget $TEMPEST_CONFIG compute image_ref`
-iniset $TEMPEST_CONFIG compute image_ref_alt $IMAGE_REF
-
-iniset $TEMPEST_CONFIG compute build_timeout 180
-iniset $TEMPEST_CONFIG orchestration build_timeout 180
-iniset $TEMPEST_CONFIG volume build_timeout 180
-iniset $TEMPEST_CONFIG boto build_timeout 180
-
-iniset $TEMPEST_CONFIG compute ssh_timeout 180
-iniset $TEMPEST_CONFIG compute allow_tenant_isolation True
